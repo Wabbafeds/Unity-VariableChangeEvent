5e8d85394705de502698d5eff2c8ce7aac5f8a9e
Wabbafeds <bifede98@gmail.com>
2024-01-24 19:57:11 +0100

    add contructors
    added contructors and now the events get triggered even if the new assigned value is equal to the old one

--- VariableChangeEvent.cs
+++ VariableChangeEvent.cs
@@ -2,28 +2,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 [System.Serializable]
 public class VariableChangeEvent<T>
 {
+    #region Constructors
+    public VariableChangeEvent(T variable)
+    {
+        _value = variable;
+    }
+    public VariableChangeEvent() { }
+    #endregion
+
     [SerializeField] private T _value;
 
     public ValueGetBothEvent OnValueChangedGetBoth = new();
     public ValueGetEvent OnValueChangedGet = new();
     public ValueEvent OnValueChanged = new();
 
-    // Property to get and set the value, triggering the event when set
+    // Property to get and set the value, triggering the events when set
     public T Value
     {
         get { return _value; }
         set
-        {
-            if (!EqualityComparer<T>.Default.Equals(_value, value))
-            {
-                OnValueChangedGetBoth.Invoke(_value, value);
-                _value = value;
-                OnValueChangedGet.Invoke(_value);
-                OnValueChanged.Invoke();
-            }
+        {               
+            OnValueChangedGetBoth.Invoke(_value, value);
+            _value = value;
+            OnValueChangedGet.Invoke(_value);
+            OnValueChanged.Invoke();       
+        }
+    }
+
+    //Property to set the value without triggering the events
+    public T ValueNoEventTrigger
+    {
+        set
+        {              
+            _value = value;
         }
     }
 
@@ -34,4 +49,5 @@ public class VariableChangeEvent<T>
     [System.Serializable]
     public class ValueEvent : UnityEvent { }
 }
+
 
